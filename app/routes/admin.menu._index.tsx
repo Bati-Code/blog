d66bc4e322abe9c65204b5153e4dae47fdc4e4fc
DESKTOP-UL97IHH\admin <dbc200188@gmail.com>
2023-08-29 14:20:37 +0900

    post

--- app/routes/admin.menu._index.tsx
+++ app/routes/admin.menu._index.tsx
@@ -1,7 +1,25 @@
 import { db } from '~/utils/db.server';
 import { json } from 'react-router';
+import { ActionArgs, ActionFunction } from '@remix-run/node';
 
 export const loader = async () => {
   const count = await db.menu.count();
   return json({ count: count });
+};
+
+export const action: ActionFunction = async ({ request }: ActionArgs) => {
+  const body = await request.json();
+
+  try {
+    db.menu.create({
+      data: {
+        menuId: body.id,
+        name: body.name,
+      },
+    });
+    return json({ count: 1 }, 400);
+  } catch (error) {
+    console.log('Error');
+    throw error;
+  }
 };
